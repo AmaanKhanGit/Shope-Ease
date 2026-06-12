f57bad1e6476ec908cd38f48d659e89919b8544e
AmaanKhanGit <amaan050606@gmail.com>
2026-06-12 19:57:36 +0530

    user data har code

---
+++ src/Userdata/userdata.js
@@ -0,0 +1,52 @@
+userData = [
+  {
+    idx: 0,
+    username: "emilys",
+    password: "emilyspass",
+  },
+  {
+    idx: 1,
+    username: "michaelw",
+    password: "michaelwpass",
+  },
+  {
+    idx: 2,
+    username: "sophiab",
+    password: "sophiabpass",
+  },
+  {
+    idx: 3,
+    username: "jamesd",
+    password: "jamesdpass",
+  },
+  {
+    idx: 4,
+    username: "emmaj",
+    password: "emmajpass",
+  },
+  {
+    idx: 5,
+    username: "oliviaw",
+    password: "oliviawpass",
+  },
+  {
+    idx: 6,
+    username: "alexanderj",
+    password: "alexanderjpass",
+  },
+  {
+    idx: 7,
+    username: "avat",
+    password: "avatpass",
+  },
+  {
+    idx: 8,
+    username: "ethanm",
+    password: "ethanmpass",
+  },
+  {
+    idx: 9,
+    username: "isabellad",
+    password: "isabelladpass",
+  },
+];
