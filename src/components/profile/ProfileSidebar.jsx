6faabb276c2b6d7bbd738bd9f25f9426afb718da
AmaanKhanGit <amaan050606@gmail.com>
2026-06-13 14:48:43 +0530

    profile sidebar

--- src/components/profile/ProfileSidebar.jsx
+++ src/components/profile/ProfileSidebar.jsx
@@ -1,5 +1,36 @@
+import "./ProfileSidebar.css";
+
 const ProfileSidebar = () => {
-  return <h1>ProfileSidebar</h1>;
+  return (
+    <aside className="profile-sidebar">
+      <div className="sidebar-links">
+        <button className="sidebar-link">
+          <i className="bi bi-grid"></i>
+          <span>Dashboard</span>
+        </button>
+
+        <button className="sidebar-link">
+          <i className="bi bi-heart"></i>
+          <span>Wishlist</span>
+        </button>
+
+        <button className="sidebar-link">
+          <i className="bi bi-cart3"></i>
+          <span>Cart</span>
+        </button>
+
+        <button className="sidebar-link active">
+          <i className="bi bi-person"></i>
+          <span>Profile</span>
+        </button>
+      </div>
+
+      <button className="sidebar-link logout-btn">
+        <i className="bi bi-box-arrow-right"></i>
+        <span>Logout</span>
+      </button>
+    </aside>
+  );
 };
 
 export default ProfileSidebar;
