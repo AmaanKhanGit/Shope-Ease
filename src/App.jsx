3d3f3828e361f747f93724c7ad68744e0785be3e
AmaanKhanGit <amaan050606@gmail.com>
2026-06-13 15:48:21 +0530

    testing phase

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Footer from "./components//layout/Footer";
 import Header from "./components/layout/Header";
 import { Outlet, ScrollRestoration } from "react-router-dom";
 
+//& the development phase has been done now it's in testing phase
+
 function App() {
   return (
     <>
